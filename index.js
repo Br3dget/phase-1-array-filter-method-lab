8a2f9cc50df201c58a966c9f949748b5afe42309
Bridget <bridget.njoki@student.moringaschool.com>
2024-07-05 10:11:15 +0300

    Add filter

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution here
+const findMatching = (drivers, name) => {
+    return drivers.filter(driver=> driver.lowerCase()===name.tOLowerCase());
+};
+
+const fuzzyMatch = (drivers, letters) => {
+    return drivers.filter(driver => driver.toLoweCase().startsWith(letters.toLoweCase()));
+};
+
+const matchName = (drivers, name) => {
+    return drivers.filter(driver => driver .name.tOLowerCase()=== name.tOLowerCase());
+};
+
+const drivers = [
+    { name: "Bobby", hometown: "Bungoma"},
+    { name: "Brianna", hometown: "Nakuru"},
+    { name: "Kevin", hometown: "Eldoret"},
+    { name: "Mary", hometown: "Nairobi"},
+
+];
+
+console.log(findMatching(drivers.map(driver => driver.name), "Bobby"));
+console.log(fuzzyMatch(drivers.map(driver => driver.name), "Kevin"));
+console.log(matchName(drivers, "Mary"));
